2f712261a176940cfad45ffbafb90c49a54a5a90
Azimkhan018 <azimkhanx09@gmail.com>
2025-06-05 10:34:17 +0530

    i learned function

--- hello.js
+++ hello.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+// console.log("hello world");
+// const prompt = require('prompt-sync')();
+// const name = prompt("What is your name? ");
+// console.log("Hello, " + name + "!");
+const sq=function (x=50){
+    let ans=0;
+    ans=x*x;
+    return ans;
+}
+console.log("the answer is:"+sq());
+
