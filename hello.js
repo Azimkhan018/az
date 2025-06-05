7206aef1e1dbde969138fe402b81a36da0c5e524
Azimkhan018 <azimkhanx09@gmail.com>
2025-06-05 11:16:58 +0530

    imp commit

--- hello.js
+++ hello.js
@@ -1,11 +1,11 @@
-// console.log("hello world");
-// const prompt = require('prompt-sync')();
-// const name = prompt("What is your name? ");
-// console.log("Hello, " + name + "!");
-const sq=function (x=50){
-    let ans=0;
-    ans=x*x;
-    return ans;
+
+function greet(name,cb){
+    console.log("hello " + name + "!");
+    cb();
 }
-console.log("the answer is:"+sq());
+  function vclg(){
+    console.log("goodbye");
+}
+greet("azim",vclg);
+
 
